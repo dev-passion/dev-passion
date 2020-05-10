b5bfacce677252137f83836d3bafa287532f9960
talhamuhammadashraf <talhamuhammadashraf@gmail.com>
2020-05-10 17:40:40 +0500

    Updates

---
+++ precache-manifest.ae8e06d4b61326617d9925767654395e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fed46eceafbb05a12730faf576b3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d0f7d969eb2fe23e51f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5f9efbd29dcd094c89f",
+    "url": "/static/js/2.c1b6e0f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1b6e0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0f7d969eb2fe23e51f",
+    "url": "/static/js/main.cf32eed9.chunk.js"
+  },
+  {
+    "revision": "22e9bc03d47a13a25f85",
+    "url": "/static/js/runtime-main.05013c3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
